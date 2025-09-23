4a430ff273e44eb19798982b47f4399a102f2185
taeseok <tsk941229@gmail.com>
2025-09-24 00:37:36 +0900

    feat: AppicationContext 객체에서 컨테이너가 관리하는 Bean의 Names 뽑아보기

--- src/main/java/com/springtour/example/chapter02/Chapter02Application.java
+++ src/main/java/com/springtour/example/chapter02/Chapter02Application.java
@@ -1,13 +1,28 @@
 package com.springtour.example.chapter02;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootApplication
 public class Chapter02Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Chapter02Application.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Chapter02Application.class, args);
+
+        Environment env = ctx.getBean(Environment.class);
+        String port = env.getProperty("server.port");
+        log.info("Customize Port: {}", port);
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.stream(beanNames).forEach(beanName -> log.info("Bean Name : {}", beanName));
+
+
     }
 
 }
