e7d8c6839fb06bdce4751fa7f965e14fa7af1532
taeseok <tsk941229@gmail.com>
2025-10-27 21:30:19 +0900

    note: REST-API Post method 실습

--- src/main/java/com/springtour/example/chapter05/controller/HotelRoomController.java
+++ src/main/java/com/springtour/example/chapter05/controller/HotelRoomController.java
@@ -2,17 +2,28 @@ package com.springtour.example.chapter05.controller;
 
 import com.springtour.example.chapter05.domain.HotelRoomType;
 import com.springtour.example.chapter05.domain.dto.DeleteResultResponse;
+import com.springtour.example.chapter05.domain.dto.HotelRoomIdResponse;
+import com.springtour.example.chapter05.domain.dto.HotelRoomRequest;
 import com.springtour.example.chapter05.domain.dto.HotelRoomResponse;
 import com.springtour.example.chapter05.utils.IdGenerator;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 @RestController
 public class HotelRoomController {
+
+    private static final String HEADER_CREATED_AT = "X-CREATED-AT";
+    private final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
 
     @GetMapping(path = "/hotels/{hotelId}/rooms/{roomNumber}")
     public HotelRoomResponse getHotelRoomByPeriod(
@@ -37,6 +48,20 @@ public class HotelRoomController {
             @PathVariable String roomNumber) {
         System.out.println("Delete Request. hotelId=" + hotelId + ", roomNumber=" + roomNumber);
         return new DeleteResultResponse(Boolean.TRUE, "success");
+    }
+
+    @PostMapping(path = "/hotels/{hotelId}/rooms")
+    public ResponseEntity<HotelRoomIdResponse> createHotelRoom(
+            @PathVariable Long hotelId,
+            @RequestBody HotelRoomRequest hotelRoomRequest
+    ) {
+        System.out.println(hotelRoomRequest.toString());
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add(HEADER_CREATED_AT, DATE_FORMATTER.format(ZonedDateTime.now()));
+        HotelRoomIdResponse body = HotelRoomIdResponse.from(1_002_003_004L);
+
+        return new ResponseEntity(body, headers, HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/springtour/example/chapter05/domain/dto/HotelRoomRequest.java
@@ -0,0 +1,17 @@
+package com.springtour.example.chapter05.domain.dto;
+
+import com.springtour.example.chapter05.domain.HotelRoomType;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@ToString
+public class HotelRoomRequest {
+
+    private String roomNumber;
+    private HotelRoomType roomType;
+    private BigDecimal originalPrice;
+
+}
